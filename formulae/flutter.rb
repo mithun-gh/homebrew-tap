62c33c79901cde0ee9f0b534befde5f6a6b5a0d8
Mithun <mithunrathinasamy@live.com>
2019-06-18 22:11:49 +1000

    update formulae/flutter.rb

--- formulae/flutter.rb
+++ formulae/flutter.rb
@@ -8,7 +8,14 @@ class Flutter < Formula
   end
 
   def install
-    bin.install "flutter"
+    system "./bin/flutter"
+    allfiles = File.join(buildpath, "**", "{*,.*}")
+    mv Dir.glob(allfiles), Dir.glob(prefix), :force => true
+  end
+
+  def post_install
+    rm File.join(HOMEBREW_PREFIX, "bin", "flutter.bat")
+    chmod_R "+rwx", File.join(prefix, "bin")
   end
 
 end
